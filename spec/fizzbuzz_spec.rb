5d2f594297c5c3d4f4805968bf609e30237a745b
Koma Ryu <ryusoccerplayer.516@gmail.com>
2024-08-20 17:55:00 +0900

    今度こそ７の倍数の時にGitHubと出力されます

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,4 +20,10 @@ describe Fizzbuzz do
     it { subject.calculate(15).should eq 'fizzbuzz' }
     it { subject.calculate(30).should eq 'fizzbuzz' }
   end
+  context 'GitHub number' do
+    it { subject.calculate(21).should eq 'GitHub'}
+    it { subject.calculate(35).should eq 'GitHub'}
+    it { subject.calculate(42).should eq 'GitHub'}
+    it { subject.calculate(63).should eq 'GitHub'}
+  end
 end
